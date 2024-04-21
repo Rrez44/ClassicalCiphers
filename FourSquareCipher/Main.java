ff237938802d7ff58a5e1d88f6862ddddfdcdeb1
Rrezon <112092689+Rrez44@users.noreply.github.com>
2024-04-21 15:18:32 +0200

    Update Main.java

--- FourSquareCipher/Main.java
+++ FourSquareCipher/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        FourSquareCipher test = new FourSquareCipher("SECRET" , "KEYWORD");
-        System.out.println(test.encrypt("HELLOWORLD"));
-        System.out.println(test.decrypt(test.encrypt("HELLOWORLD")));
+        FourSquareCipher fsc = new FourSquareCipher();
+        fsc.run();
     }
 }
